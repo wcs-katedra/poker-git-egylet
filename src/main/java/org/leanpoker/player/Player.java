2f524a05ea67d50c7e30af2a2d340bc87a0eb628
Kalácska Patrik <p.kalacska@gmail.com>
2015-03-09 20:04:15 +0100

    Aktuális játékos lekérése megtörtént!

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -1,15 +1,30 @@
 package org.leanpoker.player;
 
 import com.wcs.poker.gamestate.GameState;
+import com.wcs.poker.jsonconverter.JsonConverter;
+import java.util.List;
 
 public class Player {
 
     static final String VERSION = "Default Java folding player";
+    JsonConverter<Object> json;
+    static int actualGamerId;
+    static com.wcs.poker.gamestate.Player actualGamer;
+    static List<com.wcs.poker.gamestate.Player> gamers;
 
+    
+    public static com.wcs.poker.gamestate.Player actualGamer(GameState gameState){
+        actualGamerId=gameState.getInAction();
+        gamers=gameState.getPlayers();
+        actualGamer=gamers.get(actualGamerId);
+        return actualGamer;
+    }
+    
     public static int betRequest(GameState gameState) {
         return 0;
     }
 
     public static void showdown(GameState gameState) {
     }
+    
 }
