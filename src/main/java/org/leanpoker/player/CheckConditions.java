b94f8bb39a4f92b8cf9dff54aa7eb559162dd942
unknown <poker04@A109-04.itlab.pmmk.pte.hu>
2015-03-11 18:00:59 +0100

    #6 Kézben lévő kártyák vizsgálata
    parádés!

---
+++ src/main/java/org/leanpoker/player/CheckConditions.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.leanpoker.player;
+
+import com.wcs.poker.gamestate.Card;
+import com.wcs.poker.gamestate.GameState;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ *
+ * @author poker10
+ */
+public class CheckConditions {
+
+    private double randNumber;
+    private int bet;
+    private GameState gameState;
+    private List<String> bigRank = Arrays.asList("10", "J", "Q", "K", "A");
+
+    public CheckConditions(GameState gameState) {
+        this.gameState = gameState;
+    }
+
+    public int check() {
+        randNumber = Math.random();
+        List<Card> cards = gameState.requestActHoleCards(gameState);
+
+        if (randNumber > 0.1) {
+            bet = 10;
+        } else {
+            bet = 0;
+        }
+        return bet;
+    }
+
+    private boolean isPair(List<Card> cards) {
+        return cards.get(0).getRank().equals(cards.get(1).getRank());
+    }
+
+    private boolean isEqualColor(List<Card> cards) {
+        return cards.get(0).getSuit().equals(cards.get(1).getSuit());
+    }
+
+    private boolean isOneBigCard(List<Card> cards) {
+        return bigRank.contains(cards.get(0).getRank()) || bigRank.contains(cards.get(1).getRank());
+    }
+
+    private boolean isTwoBigCard(List<Card> cards) {
+        return bigRank.contains(cards.get(0).getRank()) && bigRank.contains(cards.get(1).getRank());
+    }
+
+    private int howManyBigCard(List<Card> cards) {
+        int i = 0;
+        if (bigRank.contains(cards.get(0).getRank())) {
+            i++;
+        }
+        if (bigRank.contains(cards.get(1).getRank())) {
+            i++;
+        }
+        return i;
+    }
+
+}
--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -7,7 +7,7 @@ public class Player {
     static final String VERSION = "Zeusz 1.0";
 
     public static int betRequest(GameState gameState) {
-        CheckRandomNumber checkRandomNumber = new CheckRandomNumber();
+        CheckConditions checkRandomNumber = new CheckConditions(gameState);
         return checkRandomNumber.check();
     }
 
