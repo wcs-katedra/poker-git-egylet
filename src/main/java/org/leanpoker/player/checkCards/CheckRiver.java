bcfcf49355aa613dc7fe98bc2084e2f58491e1cf
Kalácska Patrik <p.kalacska@gmail.com>
2015-03-31 18:04:06 +0200

    #13 Ellenfelek lehetőségeinek vizsgálata, Drill és Royal Flush vizsgálva, tesztelve.

---
+++ src/main/java/org/leanpoker/player/checkCards/CheckRiver.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.leanpoker.player.checkCards;
+
+import com.wcs.poker.gamestate.Card;
+import com.wcs.poker.gamestate.GameState;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author Patrik
+ */
+public class CheckRiver {
+    private GameState gState;
+    private CheckOnePair cPair=new CheckOnePair();
+    private String[] rankArray={"A","K","Q","J","10"};
+    private String[] suitArray={"spades","hearts","diamonds","clubs"};
+    private int count=0;
+
+    public CheckRiver(GameState gState) {
+        this.gState = gState;
+    }
+    
+    
+    
+    public Hand mayCombination(){
+        Hand combination=null;
+        CheckResult result=cPair.getResult(gState.getCommunityCards());
+        
+        if (result!=null) {
+            combination=Hand.DRILL;
+        }
+        
+        
+        for (String suit : suitArray) {
+            for (String rank : rankArray) {
+                for (Card communityCard : gState.getCommunityCards()) {
+                    if (communityCard.equals(rank, suit)){
+                        count++;
+                    }
+                }
+            }
+            if (count==4) {
+                combination=Hand.ROYAL_FLUSH;
+            }
+            count=0;
+        }
+        
+        return combination;
+    
+}
+}
---
+++ src/test/java/org/leanpoker/player/CheckRiverTest.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.leanpoker.player;
+
+import com.wcs.poker.gamestate.GameState;
+import org.git_egylet.tools.Tools;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.leanpoker.player.checkCards.CheckRiver;
+import org.leanpoker.player.checkCards.Hand;
+
+/**
+ *
+ * @author Patrik
+ */
+public class CheckRiverTest {
+    
+    @Test
+    public void testMayPair(){
+        GameState gameState=new GameState(10, 320, 400, 241, 1, 7, 1);
+        gameState.setCommunityCards(Tools.makeCardList("2,hearts", "2,clubs", "Q,diamonds", "6,spades", "7,diamonds"));
+        
+        CheckRiver cRiver=new CheckRiver(gameState);
+        
+        assertTrue(cRiver.mayCombination()==Hand.DRILL);
+    }
+    
+    @Test
+    public void testMayRoyalFlush(){
+        GameState gameState=new GameState(10, 320, 400, 241, 1, 7, 1);
+        gameState.setCommunityCards(Tools.makeCardList("10,hearts", "J,hearts", "Q,hearts", "K,hearts", "7,diamonds"));
+        
+        CheckRiver cRiver=new CheckRiver(gameState);
+        
+        assertTrue(cRiver.mayCombination()==Hand.ROYAL_FLUSH);
+    }
+    
+}
